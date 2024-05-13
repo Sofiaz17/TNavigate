35e9c2d3527e65aa4f03e3a0c38ba88595cd4f99
Sofiaz17 <sofia.zandona@studenti.unitn.it>
2024-05-13 15:25:02 +0200

    fixed bugs

--- static/script.js
+++ static/script.js
@@ -149,7 +149,7 @@ function triggerOnEnter(){
 }
 
 async function searchShopByName(userInput) {           //called 4 times before fetching
-    console.log("searchShopByName called");
+                console.log("searchShopByName called");
 
     const ul = document.getElementById('inputSearch'); 
 
@@ -157,32 +157,32 @@ async function searchShopByName(userInput) {           //called 4 times before f
     let fetchUrl;
     if(userInput === undefined)
         userInput = document.getElementById('shopName').value.toLowerCase();
-    console.log('userInput: ' + userInput);
+                console.log('userInput: ' + userInput);
     let isCat = await isCategory(userInput);
-    console.log('iscateg value:'+ isCat);
-    console.log('isCategory(userInput)==true: ', isCat==true);
+                console.log('iscateg value:'+ isCat);
+                console.log('isCategory(userInput)==true: ', isCat==true);
     if(isCat==true){
-        console.log('fetch category url');
+                console.log('fetch category url');
         fetchUrl = '../api/v1/shops?category=' + userInput;
     } else {
-        console.log('fetch name url');
-        console.log('userinput in fetch name: ',capitalizeFirstLetter(userInput));
+                console.log('fetch name url');
+                console.log('userinput in fetch name: ',capitalizeFirstLetter(userInput));
         fetchUrl = '../api/v1/shops?name=' + capitalizeFirstLetter(userInput);
     }
 
     fetch(fetchUrl)
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(data) { // Here you get the data to modify as you please
-        console.log('enter func data Searchbyname');
+                console.log('enter func data Searchbyname');
         // console.log(data);
         // Sort the data array alphabetically based on the category names
-        //data.sort((a, b) => a.name.localeCompare(b.name));
-        console.log('before map data: ', data);
+        data.sort((a, b) => a.name.localeCompare(b.name));
+                console.log('before map data: ', data);
         
         return data.map(function(shop) { // Map through the results and for each run the code below
-            console.log('enter return');
-            console.log('fetched data: ', shop);
-            // let bookId = book.self.substring(book.self.lastIndexOf('/') + 1);
+                console.log('enter return');
+                console.log('fetched data: ', shop);
+                // let bookId = book.self.substring(book.self.lastIndexOf('/') + 1);
             
             let li = document.createElement('li');
             let span = document.createElement('span');
@@ -206,64 +206,144 @@ async function searchShopByName(userInput) {           //called 4 times before f
     .catch( error => console.error(error) );// If there is any error you will catch them here
     
 }
-  
+  //doesnt work multiple fetching
+// async function searchShopByName(userInput) {           //called 4 times before fetching
+//     console.log("searchShopByName called");
+
+// const ul = document.getElementById('inputSearch'); 
+
+// ul.textContent = '';
+// let fetchUrl;
+// if(userInput === undefined)
+// userInput = document.getElementById('shopName').value.toLowerCase();
+//     console.log('userInput: ' + userInput);
+//     console.log('typeof userInput: ', typeof(userInput));
+// let isCat = await isCategory(userInput);
+//     console.log('iscateg value:'+ isCat);
+//     console.log('isCategory(userInput)==true: ', isCat==true);
+// if(isCat==true){
+//     console.log('fetch category url');
+//     let urls = userInput.map(function(categ){
+//         fetchUrl = '../api/v1/shops?category=' + categ;
+//     })
+//     console.log('multiple fetch url: ', urls);
+// } else {
+//     console.log('fetch name url');
+//     console.log('userinput in fetch name: ',capitalizeFirstLetter(userInput));
+// fetchUrl = '../api/v1/shops?name=' + capitalizeFirstLetter(userInput);
+// }
+
+// fetch(fetchUrl)
+// .then((resp) => resp.json()) // Transform the data into json
+// .then(function(data) { // Here you get the data to modify as you please
+//     console.log('enter func data Searchbyname');
+// // console.log(data);
+// // Sort the data array alphabetically based on the category names
+// data.sort((a, b) => a.name.localeCompare(b.name));
+//     console.log('before map data: ', data);
+
+// return data.map(function(shop) { // Map through the results and for each run the code below
+//     console.log('enter return');
+//     console.log('fetched data: ', shop);
+//     // let bookId = book.self.substring(book.self.lastIndexOf('/') + 1);
+
+// let li = document.createElement('li');
+// let span = document.createElement('span');
+// // span.innerHTML = `<a href="${book.self}">${book.title}</a>`;
+// let a = document.createElement('a');
+// a.href = shop.self
+// a.textContent = shop.name+ ', ' + shop.address;
+// // span.innerHTML += `<button type="button" onclick="takeBook('${book.self}')">Take the book</button>`
+//  //let button = document.createElement('button');
+// // button.type = 'button'
+// // button.onclick = ()=>takeShop(category.self)
+// // button.textContent = 'Take the category';
+
+// // Append all our elements
+// span.appendChild(a);
+// //  span.appendChild(button);
+// li.appendChild(span);
+// ul.appendChild(li);
+// })
+// })
+// .catch( error => console.error(error) );// If there is any error you will catch them here
+
+// }
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 async function isCategory(input) {
-    console.log('isCategory entered');
+                console.log('isCategory entered');
 
     return await fetch('../api/v1/shopCategories')
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(data) { 
        // console.log('isCategory data: ' + data.name);
     //const categories = ['supermercato', 'farmacia', 'abbigliamento', 'ferramenta', 'elettronica', 'ristorazione', 'alimentari', 'sport', 'cartoleria'];
-        const category = data.find(categ => categ.name.toLowerCase() == input);
-        console.log('CATEGORY EXISTING: ', category);
-        console.log('category !== undefined: ', category !== undefined);
+
+            const category = data.find(categ => categ.name.toLowerCase() == input);
+                console.log('CATEGORY EXISTING: ', category);
+                console.log('category !== undefined: ', category !== undefined);
         if(category !== undefined){
             return true;
         } else {
             return false;
         }
-             
     })
 }
+// async function isCategory(input) {
+//         console.log('isCategory entered');
+
+//     return await fetch('../api/v1/shopCategories')
+//         .then((resp) => resp.json()) // Transform the data into json
+//         .then(function(data) { 
+//             // console.log('isCategory data: ' + data.name);
+//             //const categories = ['supermercato', 'farmacia', 'abbigliamento', 'ferramenta', 'elettronica', 'ristorazione', 'alimentari', 'sport', 'cartoleria'];
+//             const category = data.find(categ => categ.name.toLowerCase() == input[0]);
+//                     console.log('input[0]: ', input[0]);
+//                     console.log('CATEGORY EXISTING: ', category);
+//                     console.log('category !== undefined: ', category !== undefined);
+//         if(category !== undefined){
+//             return true;
+//         } else {
+//             return false;
+//         }
+//         })
+// }
 
 async function searchShopByProduct() {           //called 4 times before fetching
-    console.log("searchShopByProduct called");
+                console.log("searchShopByProduct called");
 
     const ul = document.getElementById('inputProductSearch'); 
 
     ul.textContent = '';
 
     const categToSearch = await prodCategory();
-    console.log('categtosearch: ' + categToSearch);
+                console.log('categtosearch: ' + categToSearch);
 
     searchShopByName(categToSearch);
     
 }
 
 async function prodCategory() {           //called 4 times before fetching
-    console.log("prodCategory called");
+                console.log("prodCategory called");
 
     const ul = document.getElementById('inputProductSearch'); 
 
     ul.textContent = '';
     
     const userInput = document.getElementById('productName').value.toLowerCase();
-    console.log('userInput: ' + userInput);
-    console.log('encoded uri userinput: ' + encodeURI(userInput.toLowerCase()));
+                console.log('userInput: ' + userInput);
+                console.log('encoded uri userinput: ' + encodeURI(userInput.toLowerCase()));
     
    
     return await fetch('../api/v1/products?name=' + encodeURI(userInput.toLowerCase()))
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(data) { // Here you get the data to modify as you please
-        return data.map(function(prod) { 
-            console.log('product data category: ', prod.category);
-            return prod.category;
-        })})}
+        return data.map(prod => prod.category);
+             
+        })}
         //console.log('enter func data Searchbyprod');
         // console.log(data);
         // Sort the data array alphabetically based on the category names
